fc25b2beeae6d3836560e0ee9398787e31676640
abdulazeez babatunde abdulrazaq <abdulazeezbabatunde10@gmail.com>
2024-01-02 15:08:50 -0800

    updated

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-def magic_calculation(x, y, z):
-    if x < y:
-        return z
-    if z > y:
-        return x + y
-    return x * y - z
-
+def magic_calculation(a, b, c):
+    if a < b:
+        return (c)
+    if c > b:
+        return (a + b)
+    return (a*b - c)
