e1c1a901faf143a19ec6ecb0122389eeac7ca817
abdulazeez babatunde abdulrazaq <abdulazeezbabatunde10@gmail.com>
2024-01-02 11:31:29 -0800

    update

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-# Iterate through ASCII values for lowercase letters and print them without newlines
 for char_code in range(ord('a'), ord('z') + 1):
     print('{:c}'.format(char_code), end='')
