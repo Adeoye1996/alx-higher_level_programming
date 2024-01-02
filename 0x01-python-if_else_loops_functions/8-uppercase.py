2dcb8d29d22f6ce285b73c37c0957327d34e1a59
abdulazeez babatunde abdulrazaq <abdulazeezbabatunde10@gmail.com>
2024-01-02 14:38:28 -0800

    update

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def convert_to_uppercase(input_string):
+    for char in input_string:
+        if ord(char) >= 97 and ord(char) <= 122:
+            char = chr(ord(char) - 32)
+        print("{}".format(char), end="")
+    print()
