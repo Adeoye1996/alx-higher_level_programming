2f5e9cb34a4de28c0a805337539c572bcdbe4d93
abdulazeez babatunde abdulrazaq <abdulazeezbabatunde10@gmail.com>
2024-01-04 09:11:46 -0800

    module 1

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    # Assign values to variables a and b on separate lines
+     """Print the sum of 1 and 2."""
+        from add_0 import add
+
     a = 1
     b = 2
-
-    # Import the add function from add_0.py
-    from add_0 import add
-
-    # Use print function with string format to display integers
     print("{} + {} = {}".format(a, b, add(a, b)))
+
