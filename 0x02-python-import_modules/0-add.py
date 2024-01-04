429e7ea0583de1544b1fa83f138c017892ed4e4d
abdulazeez babatunde abdulrazaq <abdulazeezbabatunde10@gmail.com>
2024-01-04 09:03:41 -0800

    module 1

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Calculate and print the sum of 1 and 2 using a custom addition function."""
+    # Assign values to variables a and b on separate lines
+    a = 1
+    b = 2
+
+    # Import the add function from add_0.py
     from add_0 import add
 
-    first_number = 1
-    second_number = 2
-    result = add(first_number, second_number)
-
-    print("{} + {} = {}".format(first_number, second_number, result))
+    # Use the print function with string format to display integers
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
