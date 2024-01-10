17d366187028a38467b805aa352e4ee319ed18d8
abdulazeez babatunde abdulrazaq <abdulazeezbabatunde10@gmail.com>
2024-01-10 11:39:14 -0800

    update

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,10 +4,5 @@ def search_replace(my_list, search, replace):
     """Search and replace elements in a list."""
     if my_list is None:
         return
-
-    for i in range(len(my_list)):
-        if my_list[i] == search:
-            my_list[i] = replace
-
-    return my_list
-
+    updated_list = [replace if element == search else element for element in my_list]
+    return updated_list
