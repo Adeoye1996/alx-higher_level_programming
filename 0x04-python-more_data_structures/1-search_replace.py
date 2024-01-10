da82a845bb70ba673f1cdf3c45aaded2a65245f1
abdulazeez babatunde abdulrazaq <abdulazeezbabatunde10@gmail.com>
2024-01-10 12:00:25 -0800

    upto

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
+
 def search_replace(my_list, search, replace):
-    n_list = list(my_list)
-    for i in range(len(n_list)):
-        if n_list[i] == search:
-            n_list[i] = replace
-    return n_list
+    """Replace occurrences of 'search' with 'replace' in the list."""
+    if my_list is None:
+        return
+
+    new_list = []
+    for item in my_list:
+        if item == search:
+            new_list.append(replace)
+        else:
+            new_list.append(item)
+
+    return new_list
 
