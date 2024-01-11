ac8d4d73ed5c32a3d5dcac63bd7399dabeca7462
abdulazeez babatunde abdulrazaq <abdulazeezbabatunde10@gmail.com>
2024-01-11 08:34:00 -0800

    upto

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,16 +1,10 @@
+#!/usr/bin/python3
 def weight_average(my_list=[]):
-    """Calculate the weighted average of a list of tuples."""
     if not my_list:
         return 0
-
-    total_weighted_sum = 0
-    total_weight = 0
-
+    average = 0
+    div = 0
     for tup in my_list:
-        total_weighted_sum += tup[0] * tup[1]
-        total_weight += tup[1]
-
-    if total_weight != 0:
-        return float(total_weighted_sum / total_weight)
-    else:
-        return 0
+        average += tup[0] * tup[1]
+        div += tup[1]
+    return float(average / div)
