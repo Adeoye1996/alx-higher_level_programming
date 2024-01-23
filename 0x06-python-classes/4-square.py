a2f7a62a7a49a245e05a47ebf3ff40db89a28fce
abdulazeez babatunde abdulrazaq <abdulazeezbabatunde10@gmail.com>
2024-01-23 12:03:37 -0800

    4-square

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+"""Definition of the Square class."""
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self._size = size
+
+    @property
+    def size(self):
+        """Get the current size of the square."""
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square.
+
+        Args:
+            value (int): The new size of the square.
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("Size must be an integer")
+        elif value < 0:
+            raise ValueError("Size must be >= 0")
+        self._size = value
+
+    def area(self):
+        """Return the current area of the square."""
+        return self._size * self._size
