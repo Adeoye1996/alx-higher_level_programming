98ec6cbbf00fef347b324609f3afa38c143a1c32
abdulazeez babatunde abdulrazaq <abdulazeezbabatunde10@gmail.com>
2024-01-23 11:53:29 -0800

    3-square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
+"""Square class defination."""
 
-"""Definition of the Square class."""
 
 class Square:
-    """Body of the Square class."""
+    """Square class body."""
 
     def __init__(self, size=0):
-        """Square constructor.
-
+        """Square contructor.
         Args:
             size (int): The size of the new square.
-            """
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
@@ -18,5 +17,5 @@ class Square:
         self.__size = size
 
     def area(self):
-        """Return the area of the square."""
-        return self.__size * self.__size
+        """Return the new area of the square."""
+        return (self.__size * self.__size)
