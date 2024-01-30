63e8145ad3a0c0780effad103f8cc65012516e8e
Adeoye196 <abdulazeezbabatunde10@gmail.com>
2024-01-30 18:01:09 +0100

    locked

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """Defines a locked class."""
 
-class LockedClass:
-    """
-    Restricts the creation of new attributes in RestrictedObject
-    except for attributes named 'first_name'.
-    """
 
-    __slots__ = ["first_name"]
+class LockedClass():
+
+    """ variables declarations """
+
+    __slots__ = ('first_name')
