e6b350e61fef0614f64a2850ee0a181523ca9716
abdulazeez babatunde abdulrazaq <abdulazeezbabatunde10@gmail.com>
2024-02-01 12:34:36 -0800

    test driven

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines a function for integer addition."""
+
+
+def add_integer(a, b=98):
+    """Return the result of adding two integers.
+    Float arguments are typecasted to ints before addition is performed.
+
+Raises:
+    TypeError: If either of a or b is a non-integer and non-float. """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer or float")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer or float")
+    return int(a) + int(b)
---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Defines a name-printing function."""
+
+
+def say_my_name(first_name, last_name=""):
+    """Print a name.
+
+    Args:
+        first_name (str): The first name to print.
+        last_name (str, optional): The last name to print. Defaults to an empty string.
+
+    Raises:
+        TypeError: If either first_name or last_name are not strings.
+    """
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+
+    full_name = " ".join([part for part in [first_name, last_name] if part])
+    print("My name is {}".format(full_name))
