8c2ca3ef6f2112edb4dbb345dba12a17c05af08c
abdulazeez babatunde abdulrazaq <abdulazeezbabatunde10@gmail.com>
2024-02-01 14:28:14 -0800

    go

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,38 +1,29 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-
-=======
->>>>>>> 16ef7107f839f934cd843d2a351906b545b9759b
 """Defines a text-indentation function."""
 
 
 def text_indentation(text):
     """Print text with two new lines after each '.', '?', and ':'.
-<<<<<<< HEAD
+
     Args:
-        text (str): The text to print.
+        text (string): The text to print.
     Raises:
         TypeError: If text is not a string.
     """
     if not isinstance(text, str):
-        raise TypeError("Input text must be a string")
-=======
-    """
-    if not isinstance(text, str):
         raise TypeError("text must be a string")
->>>>>>> 16ef7107f839f934cd843d2a351906b545b9759b
 
-    index = 0
-    while index < len(text) and text[index] == ' ':
-        index += 1
+    c = 0
+    while c < len(text) and text[c] == ' ':
+        c += 1
 
-    while index < len(text):
-        print(text[index], end="")
-        if text[index] == "\n" or text[index] in ".?:":
-            if text[index] in ".?:":
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] == "\n" or text[c] in ".?:":
+            if text[c] in ".?:":
                 print("\n")
-            index += 1
-            while index < len(text) and text[index] == ' ':
-                index += 1
+            c += 1
+            while c < len(text) and text[c] == ' ':
+                c += 1
             continue
-        index += 1
+        c += 1
