686ff08e62c1788ed981af7754976348726680d6
abdulazeez babatunde abdulrazaq <abdulazeezbabatunde10@gmail.com>
2024-02-05 14:48:14 -0800

    inheritance

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,22 +1,17 @@
 #!/usr/bin/python3
+"""Defines a Rectangle subclass Square."""
+Rectangle = __import__('9-rectangle').Rectangle
 
-""" Define the square module """
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+class Square(Rectangle):
+    """Square class body."""
 
-class Rectangle(BaseGeometry):
-    """ Define the Squre class """
+    def __init__(self, size):
+        """Initialize a new square.
 
-    def __init__(self, width, height):
-        super().integer_validator("width", width)
-        super().integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """ Calculate and return the area of rectangle """
-        return self.__width * self.__height
-
-    def __str__(self):
-        """ Return a string representation of the rectangle """
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+        Args:
+            size (int): The size of the new square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
