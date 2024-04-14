0e49e5a78d1c7f5a8c91c27847858a12da3817d6
adeoye1996 <abdulazeezbabatunde10@gmail.com>
2024-04-14 09:19:23 -0700

    2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""
+Displays all values in the states table of the specified database where
+name matches the provided state name
+"""
+import sys
+import MySQLdb
+
+def main():
+    if len(sys.argv) != 5:
+        print("Usage: {} <username> <password> <database> <state_name>".format(sys.argv[0]))
+        return
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    try:
+        db = MySQLdb.connect(
+            host="localhost",
+            user=username,
+            passwd=password,
+            db=database,
+            port=3306
+        )
+    except MySQLdb.Error as e:
+        print("Error connecting to the database:", e)
+        return
+
+    cursor = db.cursor()
+
+    try:
+        query = """
+            SELECT *
+            FROM states
+            WHERE name LIKE BINARY '{}'
+            ORDER BY id ASC
+        """.format(state_name)
+        cursor.execute(query)
+        result = cursor.fetchall()
+        for row in result:
+            print(row)
+    except MySQLdb.Error as e:
+        print("Error executing query:", e)
+    finally:
+        cursor.close()
+        db.close()
+
+if __name__ == "__main__":
+    main()
