0ddd8b4877954d90e2556b03a93edcf8b2765676
adeoye1996 <abdulazeezbabatunde10@gmail.com>
2024-04-14 14:53:26 -0700

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,9 +6,11 @@ name matches the provided state name
 import sys
 import MySQLdb
 
+
 def main():
     if len(sys.argv) != 5:
-        print("Usage: {} <username> <password> <database> <state_name>".format(sys.argv[0]))
+        print("Usage: {} <username> <password> <database> <state_name>"
+              .format(sys.argv[0]))
         return
 
     username = sys.argv[1]
@@ -38,14 +40,15 @@ def main():
             ORDER BY id ASC
         """.format(state_name)
         cursor.execute(query)
-        result = cursor.fetchall()
-        for row in result:
+        results = cursor.fetchall()
+        for row in results:
             print(row)
     except MySQLdb.Error as e:
         print("Error executing query:", e)
     finally:
         cursor.close()
         db.close()
+
 
 if __name__ == "__main__":
     main()
