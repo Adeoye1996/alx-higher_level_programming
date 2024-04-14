2f934a6e79eb5feb37b51e864053909ddce49568
adeoye1996 <abdulazeezbabatunde10@gmail.com>
2024-04-14 15:52:30 -0700

    correction

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,40 +1,22 @@
 #!/usr/bin/python3
+"""List all states objects with a name containing the letter
+   a from the database hbtn_0e_6_usa
 """
-Deletes all State objects with a name containing 'a'
-"""
-
-import sys
+from sys import argv
 from model_state import Base, State
-from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
-
-    username, password, database = sys.argv[1:]
-
-    # Create MySQL connection string
-mysql = f"mysql+mysqldb://{username}:{password}@localhost:3306/{database}"
-
-# Create SQLAlchemy engine
-engine = create_engine(mysql)
-
-# Create all tables in the database
-Base.metadata.create_all(engine)
-
-# Create session
-Session = sessionmaker(bind=engine)
-session = Session()
-
-# Delete State objects with name containing
-num_deleted = session.query(State)\
-                     .filter(State.name.contains('a'))\
-                     .delete(synchronize_session=False)
-session.commit()
-
-print(f"{num_deleted} State(s) deleted.")
-
-# Close session
-session.close()
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'
+        .format(argv[1], argv[2],
+                argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).filter(State.name.like('%a%')).all()
+    for state in states:
+        session.delete(state)
+    session.commit()
+    session.close()
