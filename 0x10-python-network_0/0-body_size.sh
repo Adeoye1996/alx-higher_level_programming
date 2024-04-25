94ee0e166f20326e9a73e532a751f5f428e29dc8
adeoye1996 <abdulazeezbabatunde10@gmail.com>
2024-04-25 14:58:34 -0700

    0-body_size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# Takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -sI "$1" | grep -i content-length | awk '{print $2}'
+# Sends a request to the URL and displays the size of the response body
+curl -s "$1" | wc -c
 
