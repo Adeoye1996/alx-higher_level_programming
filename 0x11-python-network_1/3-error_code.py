3cf73fe9dec1546c4624dfd0b4d9963ffd199b3f
adeoye1996 <abdulazeezbabatunde10@gmail.com>
2024-04-27 06:48:45 -0700

    3-error

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Takes in a URL, sends a request to the URL, and displays the body of the response.
+"""
+if __name__ == "__main__":
+    import urllib.error as error
+    import urllib.request as request
+    from sys import argv
+    
+    url = argv[1]
+    try:
+        with request.urlopen(url) as response:
+            print(response.read().decode('utf-8'))
+    except error.HTTPError as e:
+        print("Error code: {}".format(e.code))
