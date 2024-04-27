f0b33e687d8f8bc0f31b4a028914756879bcdbb5
adeoye1996 <abdulazeezbabatunde10@gmail.com>
2024-04-27 13:50:55 -0700

    10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
 """
-Takes your GitHub credentials (username and personal access token) and uses the GitHub API
+Takes your GitHub credentials (username and password) and uses the GitHub API
 to display your id
 """
 
 if __name__ == '__main__':
-    import requests
-    import sys
+    from requests import get
+    from sys import argv
 
-    username = sys.argv[1]
-    token = sys.argv[2]
+    username = argv[1]
+    password = argv[2]
 
-    url = "https://api.github.com/user"
-    response = requests.get(url, auth=(username, token))
-    if response.status_code == 200:
-        print(response.json().get('id'))
-    else:
-        print("Error:", response.status_code)
+    URL = "https://api.github.com/user"
+    response = get(URL, auth=(argv[1], argv[2]))
+    json = response.json()
+
+    print(json.get('id'))
