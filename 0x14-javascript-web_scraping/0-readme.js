6bba264f6d0fc8392524d3f05d290a3856e037b2
adeoye1996 <abdulazeezbabatunde10@gmail.com>
2024-05-21 15:02:12 -0700

    work

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,48 +1,14 @@
 #!/usr/bin/node
-
 // Import the filesystem module
-// const fs = require('fs');
 //
-// // Check if a file path is provided
-// if (process.argv.length !== 3) {
-//   console.error('Error: No file path provided.');
-//     process.exit(1);
-//     }
-//
-//     // Get the file path from the first command-line argument
-//     const filePath = process.argv[2];
-//
-//     // Read the content of the file in utf-8 encoding
-//     fs.readFile(filePath, 'utf-8', (err, data) => {
+// const filesys = require('fs');
+// filesys.readFile(process.argv[2], 'utf-8',
+//   function (err, data) {
 //       if (err) {
-//           // Print the error object if an error occurred during reading
-//               console.error(err);
-//                   return;
-//                     }
-//                       // Print the content of the file
-//                         console.log(data);
-//                         });#!/usr/bin/node
-//
-//                         // Import the filesystem module
-//                         const fs = require('fs');
-//
-//                         // Check if a file path is provided
-//                         if (process.argv.length !== 3) {
-//                           console.error('Error: No file path provided.');
-//                             process.exit(1);
+//             // Print the error object if an error occurred during reading
+//                   console.log(err);
+//                         return;
 //                             }
-//
-//                             // Get the file path from the first command-line argument
-//                             const filePath = process.argv[2];
-//
-//                             // Read the content of the file in utf-8 encoding
-//                             fs.readFile(filePath, 'utf-8', (err, data) => {
-//                               if (err) {
-//                                   // Print the error object if an error occurred during reading
-//                                       console.error(err);
-//                                           return;
-//                                             }
-//                                               // Print the content of the file
-//                                                 console.log(data);
-//                                                 });
-//
+//                                 // Print the content of the file
+//                                     console.log(data);
+//                                       });
