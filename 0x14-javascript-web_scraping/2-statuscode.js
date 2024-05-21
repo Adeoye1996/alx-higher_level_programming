e2fb8e3f5e25d0a47b9f018a128ba23694453b67
Adeoye1996 <abdulazeezbabatunde10@gmail.com>
2024-05-22 00:41:29 +0100

    2-status

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-// Status Of Get request
 
 const request = require('request');
 const url = process.argv[2];
-request.get(url, function (error, response) {
+
+request.get(url, (error, response) => {
   if (error) {
-    console.log(error);
+    console.error(error);
   } else {
-    console.log('code:' + ' ' + response.statusCode);
+    console.log('code: ' + response.statusCode);
   }
 });
