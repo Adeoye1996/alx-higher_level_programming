bbe672080d435e0ec99997939be0ae537ca17b90
Adeoye1996 <abdulazeezbabatunde10@gmail.com>
2024-05-22 00:04:28 +0100

    ream

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,13 +5,8 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const content = process.argv[3];
 
-if (!filePath || !content) {
-  console.error('Error: File path or content not provided.');
-  process.exit(1);
-}
-
-fs.writeFile(filePath, content, 'utf-8', (err) => {
+fs.writeFile(filePath, content, 'utf-8', function (err) {
   if (err) {
-    console.error(err);
+    console.log(err);
   }
 });
